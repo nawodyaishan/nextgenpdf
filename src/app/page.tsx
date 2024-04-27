e818417a65e6098a1d5a15aa364a5751e12ad05c
Nawodya Ishan <nawodyain@gmail.com>
2024-04-27 23:33:27 +0530

    feat: add clerk authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,33 @@
-export default function Home() {
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { Button } from '@/components/ui/button';
+import { SignOutButton } from '@clerk/nextjs';
+
+export default async function Home() {
+  const router = useRouter();
+
+  const navigateToSignUp = () => {
+    router.push('/sign-up');
+  };
+
+  const navigateToSignIn = () => {
+    router.push('/sign-in');
+  };
+
   return (
-    <main>
-      <h1 className={'text-red-600'}>Hello</h1>
-    </main>
+    <div
+      className={
+        'min-h-screen w-screen bg-gradient-to-tr from-green-300 via-blue-500 to-purple-600'
+      }
+    >
+      <div className={'absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2'}>
+        <Button onClick={navigateToSignUp}>Sign Up</Button>
+        <Button onClick={navigateToSignUp}>Sign In</Button>
+        <Button>
+          <SignOutButton />
+        </Button>
+      </div>
+    </div>
   );
 }
--- src/app/sign-up/[[...sign-up]]/layout.tsx
+++ src/app/sign-up/[[...sign-up]]/layout.tsx
@@ -1,13 +1,11 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import './globals.css';
 import React from 'react';
-import { ClerkProvider } from '@clerk/nextjs';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'NextGenPDF',
+  title: 'Sign Up',
 };
 
 export default function RootLayout({
@@ -16,10 +14,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
-      <html lang="en">
-        <body className={inter.className}>{children}</body>
-      </html>
-    </ClerkProvider>
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
   );
 }
